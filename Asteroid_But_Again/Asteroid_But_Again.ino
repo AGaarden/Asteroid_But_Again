a77f6a88752ff0531b4ad1cc6ea717f16e7f8244
Sander Gaarden <sandergaarden@gmail.com>
2020-11-23 23:18:22 +0100

    Changed timer system from delay to millis setup. Added potentiometer controls to ship. Moved player drawing to separate function (DrawObjects). Cleaned up a bit of code.

--- Asteroid_But_Again/Asteroid_But_Again.ino
+++ Asteroid_But_Again/Asteroid_But_Again.ino
@@ -8,6 +8,14 @@
 #include <Wire.h>
 #endif
 
+#define potentioPin 36 // Pin defined for easier reading
+
+/*
+    Conversion multiplier for width of display and analog input
+    The fraction 123/4095 correlates to the maximum
+*/
+const float analogWidthConvert = 0.030037;
+
 /*
   Using SPI
   Pin  Connected to
@@ -32,6 +40,7 @@ typedef struct {
   int Height = 3;
 } meteortp1;
 
+// Building structure for ship
 typedef struct {
   int xpos = 0;
   int ypos = 0;
@@ -39,10 +48,42 @@ typedef struct {
   int Height = 2;
 } ship;
 
-/*
-   Collision detection
 
-   if(meteor.ypos < 64){
+
+// Create ship
+ship player;
+
+// Define integer for potentiometer input
+int potentioRawInput;
+int potentioInput;
+
+// Define longs for the timer
+long previousMillis = 0;
+long interval = 100;
+
+/*
+   Function: DrawObjects
+   Input: Nothing(?)
+   Output: Nothing
+   Remarks: Draws frames of the objects at this point in time
+*/
+void DrawObjects(int potentioInput) {
+  u8g2.clearBuffer();
+
+  // Player ship
+  u8g2.drawFrame(potentioInput, 63 - player.Height, player.Length, player.Height);
+
+  // Meteors
+
+
+  u8g2.sendBuffer();
+}
+
+bool CheckCollision() {
+  /*
+    Collision detection
+
+    if(meteor.ypos < 64){
     if(
     (meteor.xpos + meteor.length) > ship.xpos &&
     meteor.xpos < (ship.xpos + ship.length) &&
@@ -51,26 +92,29 @@ typedef struct {
     ){
       hit = true;
     }
-   }
+    }
 
-*/
+  */
 
-  // Create ship
-  ship player;
+  return false;
+}
 
 void setup() {
   Serial.begin(115200);
+
+  // Potentiometer pin declared
+  pinMode(potentioPin, INPUT);
 
   // Starts display without powersave mode
   u8g2.begin();
   u8g2.setPowerSave(0);
 
-  // Set font for when writing
+  // Set font for writing
   u8g2.setFont(u8g2_font_ncenB14_tr);
 
   // Introduction text
   u8g2.clearBuffer();
-  u8g2.drawStr(16, 39, "Asteroid");
+  u8g2.drawStr(16, 39, "Poggers");
   u8g2.sendBuffer();
   delay(3000);
   u8g2.clearDisplay();
@@ -78,10 +122,28 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 9; i < 117; i++) {
-    u8g2.clearBuffer();
-    u8g2.drawFrame(i, 10, player.Length, player.Height);
-    u8g2.sendBuffer();
-    delay(50);
+  // Current millis based on time since Arduino turned on
+  unsigned long currentMillis = millis();
+
+  // Starts processes if the time reaches the interval
+  if (currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis; // Sets previousMillis to be currentMillis to compare next run
+
+    // Troubleshooting code
+    int potentioRawInput = analogRead(potentioPin);
+    int potentioInput = (analogRead(potentioPin) * analogWidthConvert);
+
+    Serial.println("Raw player input: " + String(potentioRawInput));
+    Serial.println("Calculated player input: " + String(potentioInput));
+
+    // Drawing all objects on screen
+    DrawObjects(potentioInput);
+
+    // Collision check
+    bool isHit = CheckCollision();
+
+    if (isHit) {
+      // Game over code here
+    }
   }
 }
